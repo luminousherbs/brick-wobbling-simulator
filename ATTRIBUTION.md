0ba00b0677c9769fa05bbf5fd6bac16c8da41307
Lucas Hill <lucashill@Tims-MacBook-Pro.local>
2025-02-02 18:17:51 +0000

    Reorganised file structure for images. Added a <noscript> tag to index.html. Added functionality to change the image once the score reaches 100. Added support for arrow keys and a/d keys.

--- ATTRIBUTION.md
+++ ATTRIBUTION.md
@@ -1,6 +1,16 @@
 # Asset Attribution and Credits
-## Image: brick.jpg
+## Images
+### bricks/0.jpg
+![](src/images/bricks/0.jpg)
 Photo by [Andrewlister](https://commons.wikimedia.org/wiki/User:Andrewlister) licensed under the Public Domain, via [Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Brick.jpg).
+
+## favicons/brick.jpg
+![](src/images/favicons/brick.jpg)
+Adapted by [luminousherbs](https://github.com/luminousherbs), using photo by [Andrewlister](https://commons.wikimedia.org/wiki/User:Andrewlister) licensed under the Public Domain, via [Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Brick.jpg).
+
+## bricks/0_hat.jpg
+![](src/images/bricks/0_hat.png)
+Adapted by [luminousherbs](https://github.com/luminousherbs), using photo by [Andrewlister](https://commons.wikimedia.org/wiki/User:Andrewlister) licensed under the Public Domain, via [Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Brick.jpg), and photo from [Library of Congress](https://www.loc.gov/), via [Wikimedia Commons](https://commons.wikimedia.org/wiki/File:Austin_Lane_Crothers,_photograph_of_head_with_top_hat.jpg).
 
 ## All other assets, including code
 Created by [luminousherbs](https://github.com/luminousherbs), available for use under the MIT License.
--- src/scripts.js
+++ src/scripts.js
@@ -4,34 +4,58 @@
 //     alert("Hello, World!");
 // })
 
+let score = -1;
+
+const allowedKeys = new Set(["ArrowLeft", "ArrowRight", "a", "d", "A", "D"]);
+
+
 function updateScore() {
-    scoreDisplay = document.getElementById("highScore");
-    newScore = randomNumber(1000);
-    scoreDisplay.innerText = "High score: " + newScore;
-}
+    scoreElement = document.getElementById("highScore");
+    score++;
+    if (score >= 100) {
+        changeImageSource(brick, "images/bricks/0_hat.png");
+    }
+    scoreElement.innerText = "Score: " + score;
+};
 
 function randomNumber(max) {
+    // javascript moment
     return Math.floor(Math.random() * max);
-}
+};
 
 function rotateElement(element) {
     console.log("rotateElement");
     element.classList.toggle("tiltLeft");
     element.classList.toggle("tiltRight");
-}
+};
+
+function brickWasClicked() {
+        console.log("brick cliked");
+        rotateElement(brick);
+        updateScore();
+};
+
 
 function addListenerToBrick() {
     console.log("addListenerToBrick");
     updateScore();
     let brick = document.getElementById("brick");
     console.log(brick);
-    brick.addEventListener("click", function() {
-        console.log("brick cliked");
-        rotateElement(brick);
-        updateScore();
-    });
+    brick.addEventListener("click", brickWasClicked);
+};
+
+function changeImageSource(image, source) {
+    image.src = source;
 };
 
 document.addEventListener("DOMContentLoaded", addListenerToBrick);
+
+document.addEventListener("keydown", function(event) {
+    pressedKey = allowedKeys.has(event.key);
+    if (pressedKey) {
+        brickWasClicked();
+    }
+});
+
 console.log("scripts.js loaded");
 
